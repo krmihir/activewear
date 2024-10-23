03913f05c0b1a81a960df5db03309399bc6f07a9
Mihir Kumar <130930146+krmihir@users.noreply.github.com>
2024-10-23 18:02:59 +0530

    Update Cart.jsx

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { CartContext } from "../context/CartContext";
+import { CartContext } from "../Context/CartContext";
 
 const Cart = () => {
   const { cart, removeFromCart } = useContext(CartContext);
